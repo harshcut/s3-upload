a00d840a9c5a46dc5c52e81c95a000b02f56e28a
harshcut <harshskarande@gmail.com>
2023-05-28 12:11:36 +0530

    init react on client workspace

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main>
+      <button className="w-full rounded-full bg-blue-600 px-4 py-2 text-white">
+        Sign In with Google
+      </button>
+    </main>
+  )
+}
