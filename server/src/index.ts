f797d247000f85653c62e16631f273a631ccbd04
harshcut <harshskarande@gmail.com>
2023-05-31 17:36:50 +0530

    remove passport for oauth

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
 import dotenv from 'dotenv'
 import express, { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
 import cookieSession from 'cookie-session'
+import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import passport from 'passport'
-import { Strategy } from 'passport-google-oauth20'
+import { google } from 'googleapis'
 import multer from 'multer'
 import prisma from '../prisma'
 import getFileFS from './controllers/get-file-fs'
@@ -12,98 +13,67 @@ import type { User } from '@prisma/client'
 
 dotenv.config()
 const port = process.env.HTTP_PORT || 4000
+const secret = process.env.SECRET || ''
+const origin = process.env.CROSS_ORIGIN || 'http://localhost:3000'
 const upload = multer()
 
-passport.use(
-  new Strategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-      callbackURL: '/auth/google/callback',
-    },
-    async (_accessToken, _refreshToken, profile, done) => {
-      const user = await prisma.user.findUnique({ where: { googleId: profile.id } })
-      if (!user) {
-        const newUser = await prisma.user.create({
-          data: { googleId: profile.id, displayName: profile.displayName },
-        })
-        return done(null, newUser)
-      }
-      done(null, user)
-    }
-  )
+const authClient = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID || '',
+  process.env.GOOGLE_CLIENT_SECRET || '',
+  'http://localhost:4000/auth/google/callback'
 )
 
-passport.serializeUser((user, done) => {
-  done(null, (user as User).id)
-})
-
-passport.deserializeUser<string>(async (id, done) => {
-  const user = await prisma.user.findUnique({ where: { id } })
-  if (!user) {
-    const err = new Error('User not found')
-    return done(err, null)
-  }
-  done(null, user)
+const authUrl = authClient.generateAuthUrl({
+  access_type: 'offline',
+  prompt: 'consent',
+  scope: ['email', 'profile'],
 })
 
 const app = express()
-app.use(cors({ origin: process.env.CORS_ORIGIN || '*', credentials: true }))
+app.use(cors({ origin: origin, credentials: true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieSession({ keys: [process.env.SECRET || ''], maxAge: 24 * 60 * 60 * 1000 }))
-app.use((req, _, next) => {
-  if (req.session) {
-    req.session.regenerate ||= (done: () => void) => done()
-    req.session.save ||= (done: () => void) => done()
-  }
-  next()
-})
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(cookieSession({ keys: [secret], maxAge: 24 * 60 * 60 * 1000 }))
+app.use(cookieParser(secret))
 
 const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
-  if (req.isAuthenticated()) {
-    return next()
+  try {
+    req.user = jwt.verify(req.cookies['token'], secret, { ignoreExpiration: false })
+    next()
+  } catch {
+    res.status(401).json({ data: null, error: 'Unauthorized' })
   }
-  res.status(401).json({ data: null, error: 'Unauthorized' })
 }
 
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['email', 'profile'],
-  })
-)
+app.get('/auth/google', (_, res) => {
+  res.redirect(authUrl)
+})
 
-app.get(
-  '/auth/google/callback',
-  passport.authenticate('google', {
-    successRedirect: 'http://localhost:3000',
-    failureRedirect: 'http://localhost:3000',
-  }),
-  (_, res) => {
-    res.redirect('http://localhost:3000')
+app.get('/auth/google/callback', async (req, res) => {
+  const code = req.query.code as string
+  const { tokens } = await authClient.getToken(code)
+  authClient.setCredentials(tokens)
+  const { data } = await google.oauth2('v2').userinfo.get({ auth: authClient })
+  let user = await prisma.user.findUnique({ where: { googleId: data.id! } })
+  if (!user) {
+    user = await prisma.user.create({
+      data: { googleId: data.id!, displayName: data.name! },
+    })
   }
-)
+  const token = jwt.sign(user, secret)
+  res.cookie('token', token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 })
+  res.redirect(origin)
+})
 
-app.get('/logout', (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err)
-    }
-  })
-  res.redirect('http://localhost:3000')
+app.get('/logout', (_, res) => {
+  res.clearCookie('token').redirect(origin)
 })
 
 app.post('/upload', isAuthenticated, upload.array('file'), uploadFileFS)
 
 app.get('/download/:fileId', isAuthenticated, getFileFS)
 
-app.get('/', async (req, res) => {
-  if (!req.user) {
-    return res.status(401).json({ data: null, error: 'Unauthorized' })
-  }
+app.get('/', isAuthenticated, async (req, res) => {
   const user = req.user as User & { uploads?: any[] }
   user.uploads = await prisma.upload.findMany({ where: { userId: user.id } })
   res.status(200).json({ data: user, error: null })
